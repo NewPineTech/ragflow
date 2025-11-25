70857d3fe9f6706a613d0f0b672e7328251927a5
Khoi Phan <khoipm@gmail.com>
2025-11-25 13:51:51 +0700

    fix detect KB, search elastic

--- api/db/services/dialog_service.py
+++ api/db/services/dialog_service.py
@@ -1061,7 +1061,7 @@ def chatv1(dialog, messages, stream=True, **kwargs):
             prompt_config["system"] = prompt_config["system"].replace("{%s}" % p["key"], " ")
     
    
-    if len(questions) > 1 and prompt_config.get("refine_multiturn"):
+    if len(questions) > 1  #and prompt_config.get("refine_multiturn"):
         questions = [full_question(dialog.tenant_id, dialog.llm_id, messages)]
     else:
         questions = questions[-1:]
